99b73b416f1c9f38d8fee1df55c7ccb2ccf77a95
soroysoroy4 <soroysoroy4@gmail.com>
2023-02-16 10:32:07 +0800

    Export

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit'
 import carReducer from '../features/car/carSlice'
-
+//export
 export const store = configureStore({
   reducer: {
     car: carReducer
